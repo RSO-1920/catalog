e0b7828e9149d53b644ab844dc9b19a590638405
Uroš Zoretič <uz1734@student.uni-lj.si>
2019-11-09 15:26:52 +0100

    added serivce discovery

--- catalog-api/src/main/java/si/fri/rso/api/v1/CatalogApi.java
+++ catalog-api/src/main/java/si/fri/rso/api/v1/CatalogApi.java
@@ -1,8 +1,11 @@
 package si.fri.rso.api.v1;
 
+import com.kumuluz.ee.discovery.annotations.RegisterService;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+@RegisterService(value = "rso1920-catalog")
 @ApplicationPath("/v1")
 public class CatalogApi extends Application {
 }
--- services/src/main/java/si/fri/rso/services/beans/CatalogFileListBean.java
+++ services/src/main/java/si/fri/rso/services/beans/CatalogFileListBean.java
@@ -1,24 +1,33 @@
 package si.fri.rso.services.beans;
 
+import com.kumuluz.ee.discovery.annotations.DiscoverService;
 import si.fri.rso.lib.FileDTO;
 import si.fri.rso.models.converters.FileConverter;
 import si.fri.rso.models.entities.FileEntity;
 
-import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
+import java.net.URL;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
-@ApplicationScoped
+@RequestScoped
 public class CatalogFileListBean {
     @Inject
     private EntityManager em;
 
+    @Inject
+    @DiscoverService(value = "rso1920-catalog")
+    private Optional<URL> url;
+
     public List<FileDTO> getChannelFiles(Integer channelId) {
         Query q = em.createNamedQuery("selectFilesOnChannel").setParameter(1, channelId);
         List<FileEntity> channelFiles =  q.getResultList();
+
+        System.out.println("URL: " + url);
 
         return (List<FileDTO>) channelFiles.stream().map(FileConverter::toDTO).collect(Collectors.toList());
     }
